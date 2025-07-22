a1ee366caab78cd2779c863d57c01af8dcdbee38
Ankit <ankitsingh110198@gmail.com>
2025-07-22 15:44:08 +0530

    testing

--- pages/home/Treatment.tsx
+++ pages/home/Treatment.tsx
@@ -1,27 +1,29 @@
 import React, { useEffect, useRef, useState } from "react";
-import styles from "@/styles/Treatment.module.css";
+import styles from "@/styles/HappyStories.module.css";
 
 const YOUTUBE_REELS = [
   "https://www.youtube.com/embed/LjpGh3jfLn4?autoplay=1&mute=1&controls=0&loop=1&playlist=LjpGh3jfLn4",
   "https://www.youtube.com/embed/LjpGh3jfLn4?autoplay=1&mute=1&controls=0&loop=1&playlist=LjpGh3jfLn4",
   "https://www.youtube.com/embed/LjpGh3jfLn4?autoplay=1&mute=1&controls=0&loop=1&playlist=LjpGh3jfLn4",
+  "https://www.youtube.com/embed/LjpGh3jfLn4?autoplay=1&mute=1&controls=0&loop=1&playlist=LjpGh3jfLn4",
+  "https://www.youtube.com/embed/LjpGh3jfLn4?autoplay=1&mute=1&controls=0&loop=1&playlist=LjpGh3jfLn4",
+  "https://www.youtube.com/embed/LjpGh3jfLn4?autoplay=1&mute=1&controls=0&loop=1&playlist=LjpGh3jfLn4",
 ];
-
-
-
-
 
 const Treatment = () => {
   const [current, setCurrent] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    if (isHovered) return; // pause autoplay when hovering
+
     const interval = setInterval(() => {
       setCurrent((prev) => (prev + 1) % YOUTUBE_REELS.length);
     }, 3000); // autoplay every 3 seconds
 
     return () => clearInterval(interval);
-  }, []);
+  }, [isHovered]);
 
   const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
     const scrollLeft = e.currentTarget.scrollLeft;
@@ -40,11 +42,14 @@ const Treatment = () => {
         {YOUTUBE_REELS.map((url, index) => (
           <div
             className={`${styles.card} ${
-              index === current
-                ? styles.active
-                : styles.inactive
+              index === current ? styles.active : styles.inactive
             }`}
             key={index}
+            onMouseEnter={() => {
+              setCurrent(index);
+              setIsHovered(true);
+            }}
+            onMouseLeave={() => setIsHovered(false)}
           >
             <iframe
               src={url}
@@ -66,7 +71,7 @@ const Treatment = () => {
               setCurrent(i);
               containerRef.current?.scrollTo({
                 left: i * containerRef.current.offsetWidth * 0.8,
-                behavior: "smooth"
+                behavior: "smooth",
               });
             }}
           />
